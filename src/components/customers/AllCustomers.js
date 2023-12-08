519978548ffb8e62dae3cc27792eec5c49fc516e
Damian Socha <damian.socha@amigo.ie>
2023-12-08 16:17:26 +0000

    deleteActionForCustomerCompleted

--- src/components/customers/AllCustomers.js
+++ src/components/customers/AllCustomers.js
@@ -4,6 +4,8 @@ import { getCustomers } from '../../store/customersSlice';
 import SpinnerComponent from '../genericComponent/SpinnerComponent';
 import ResultComponent from '../genericComponent/ResultComponent';
 import TableComponent from '../genericComponent/TableComponent';
+import { deleteAxiosFunction } from '../../genericFunctions/axiosFunctions';
+import { isResponceSuccess } from '../../genericFunctions/functions';
 
 const AllCustomers = () => {
   const excludedKeys = ['id', 'contact', 'jobs', 'addresses'];
@@ -12,6 +14,21 @@ const AllCustomers = () => {
   const { customers, isLoading, isError, errorMessage } = useSelector(
     (state) => state.customers
   );
+
+  const detailAction = async (itemId) => {
+    console.log('From details action');
+    console.log(itemId);
+  };
+  const editAction = async (itemId) => {
+    console.log('From dedit action');
+    console.log(itemId);
+  };
+  const deleteAction = async (itemId) => {
+    const endpoint = `https://localhost:7113/api/Customer/${itemId}`;
+    const response = await deleteAxiosFunction(endpoint);
+    console.log(response);
+    //const isSuccess = isResponceSuccess(response)
+  };
 
   useEffect(() => {
     dispatch(getCustomers());
@@ -23,7 +40,13 @@ const AllCustomers = () => {
         <ResultComponent variant='danger' data={errorMessage} />
       ) : (
         customers.length > 0 && (
-          <TableComponent data={customers} excludedKeys={excludedKeys} />
+          <TableComponent
+            data={customers}
+            excludedKeys={excludedKeys}
+            detailsActionFunction={detailAction}
+            editActionFunction={editAction}
+            deleteActionFunction={deleteAction}
+          />
         )
       )}
     </div>
